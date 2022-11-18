571b2742b120c4cd5d108a9c4dcbc74b7059c70e
R01z <igorrt66@gmail.com>
2022-11-18 11:31:14 -0300

    Correções em no

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all:
+	gcc -Wall -c no.c
 	gcc -Wall -c common.c
 	gcc -Wall -lpthread equipment.c common.o -lpthread -o equipment
-	gcc -Wall -lpthread server.c common.o -lpthread -o server
+	gcc -Wall -lpthread server.c common.o no.c -lpthread -o server
 
 clean:
 	rm common.o equipment server
--- no.c
+++ no.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "no.h"
 
 equip* criaEquip(int id, int csock, struct sockaddr_storage cstorage){
-    struct equip *cdata = malloc(sizeof(*cdata));
+    equip *cdata = (equip*)malloc(sizeof(*cdata));
     cdata->csock = csock;
     memcpy(&(cdata->storage), &cstorage, sizeof(cstorage));
     cdata->id = id;
